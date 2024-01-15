126f1d94a3402c6ba9248e522c351332a71a037f
Solmihor <ihor.pavlenkov@gmail.com>
2024-01-15 13:31:49 +0200

    19 lesson finished!

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
+import Settings from "./components/Settings/Settings";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header />
-            <Navbar />
-            {/*<Profile />*/}
-            <div className="app-wrapper-content">
-                <Profile />
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                {/*<Profile />*/}
+                <div className="app-wrapper-content">
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
             </div>
-
-        </div>
+        </BrowserRouter>
     );
 };
 
