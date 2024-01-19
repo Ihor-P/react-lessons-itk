686cb3ab9ecc0e810b47c5de496b95146e5b50f5
Solmihor <ihor.pavlenkov@gmail.com>
2024-01-20 00:21:26 +0200

    23 lesson complete

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,20 +4,24 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
     return (
-        <div>
-            My posts
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
             <div className={s.posts}>
-                <Post likeCounter="15" />
-                <Post likeCounter="20" />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                <Post likeCounter="15"/>
+                <Post likeCounter="20"/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
             </div>
         </div>
     );
