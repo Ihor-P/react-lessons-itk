0fb94488dffa25f906c022a3f69bd4140aee69bd
Solmihor <ihor.pavlenkov@gmail.com>
2024-01-20 22:45:52 +0200

    24 lesson complete

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
+let postData = [
+    {id: 1, message: 'Hi, how are you?', likeCounter: 12},
+    {id: 2, message: 'It\'s my first post', likeCounter: 18},
+    {id: 3, message: 'Yo', likeCounter: 21},
+    {id: 4, message: 'Yo'},
+    {id: 5, message: 'Yo'}
+]
 const MyPosts = () => {
     return (
         <div className={s.postsBlock}>
@@ -15,13 +22,9 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post likeCounter="15"/>
-                <Post likeCounter="20"/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={postData[0].message} likeCounter={postData[0].likeCounter}/>
+                <Post message={postData[1].message} likeCounter={postData[1].likeCounter}/>
+                <Post message={postData[2].message} likeCounter={postData[2].likeCounter}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -8,7 +8,7 @@ const Post = (props) => {
                 src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8fe106ad-faf3-494f-b542-fe687bee5d8d/ddrzpun-faf6d6c4-61de-4505-926a-4a8d08c10612.jpg/v1/fill/w_400,h_289,q_75,strp/mel_gibson_braveheart__by_billyboyuk_ddrzpun-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9Mjg5IiwicGF0aCI6IlwvZlwvOGZlMTA2YWQtZmFmMy00OTRmLWI1NDItZmU2ODdiZWU1ZDhkXC9kZHJ6cHVuLWZhZjZkNmM0LTYxZGUtNDUwNS05MjZhLTRhOGQwOGMxMDYxMi5qcGciLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.mPKuXnNVwkKkWf1aMfhH7bsP88lklyMl9j6Jco-fUm0"
                 alt=""
             />
-            post 1
+            <span> {props.message}</span>
             <div>
                 <span>Like's</span>
                 <span> {props.likeCounter} </span>
