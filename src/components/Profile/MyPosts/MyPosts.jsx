3e5f70f70e6f7f7652265726cc70bde83cd48363
Solmihor <ihor.pavlenkov@gmail.com>
2024-01-23 14:10:27 +0200

    27 lesson complete, added PostArray.js and import to MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import PostArray from "../PostArray";
 
-
+let postsElements = PostArray
+    .map(p => <Post message={p.message} likeCounter={p.likeCounter}/>)
 const MyPosts = () => {
-
-    let posts = [
-        {id: 1, message: 'Hi, how are you?', likeCounter: 12},
-        {id: 2, message: 'It\'s my first post', likeCounter: 18},
-        {id: 3, message: 'Yo', likeCounter: 21},
-        {id: 3, message: 'erwrwer', likeCounter: 34},
-        {id: 3, message: 'hhhaa', likeCounter: 2423},
-        {id: 4, message: 'Yo'},
-        {id: 5, message: 'Yo'}
-    ]
-
-    let postsElements = posts
-        .map(p => <Post message={p.message} likeCounter={p.likeCounter}/>)
-
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
