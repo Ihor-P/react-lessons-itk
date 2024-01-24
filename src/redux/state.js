1fda35c6935c0ef880224ef114d7ee704fc21bdc
Solmihor <ihor.pavlenkov@gmail.com>
2024-01-24 16:20:20 +0200

    29 lesson complete

---
+++ src/redux/state.js
@@ -0,0 +1,38 @@
+// let posts =
+//
+// let dialogs =
+//
+// let messages =
+
+const state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likeCounter: 12},
+            {id: 2, message: 'It\'s my first post', likeCounter: 18},
+            {id: 3, message: 'Yo', likeCounter: 21},
+            {id: 3, message: 'erwrwer', likeCounter: 34},
+            {id: 3, message: 'hhhaa', likeCounter: 2423},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'}
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrew'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your it course?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'}
+        ]
+    }
+}
+
+export default state;
