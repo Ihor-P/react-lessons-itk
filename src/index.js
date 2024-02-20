c69d4ef3db30c786332765249983d24668a042ea
Solmihor <ihor.pavlenkov@gmail.com>
2024-02-20 12:00:42 +0200

    38 lesson complete - index.js corrected

--- src/index.js
+++ src/index.js
@@ -12,10 +12,6 @@ let rerenderEntireTree = (state) => {
         <BrowserRouter>
             <App state={state}
                  dispatch={store.dispatch.bind(store)}
-                 addPost={store.addPost.bind(store)}
-                 updateNewPostText={store.updateNewPostText.bind(store)}
-                 addMessage={store.addMessage.bind(store)}
-                 updateNewMessageText={store.updateNewMessageText.bind(store)}
             />
         </BrowserRouter>, document.getElementById('root')
     );
