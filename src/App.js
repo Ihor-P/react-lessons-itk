a876a395f9f5d5f373642ff23e8c77a64b41f33a
Solmihor <ihor.pavlenkov@gmail.com>
2024-03-18 13:09:36 +0200

    61 lesson complete (registration)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import {Route} from "react-router-dom";
 import Settings from "./components/Settings/Settings";
@@ -11,13 +10,14 @@ import OnlineContainer from "./components/Online/OnlineContainer";
 import SidebarContainer from "./components/Sidebar/SidebarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = (props) => {
     return (
 
         <div className="app-wrapper">
-            <Header />
+            <HeaderContainer />
             <Navbar />
             <div className="app-wrapper-content">
                 <Route path='/dialogs' render={() => <DialogsContainer store={props.store} />} />
